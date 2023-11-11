489cac6786f9013cca963ede1c574f7b593a692d
Lori <fushkaw@yandex.ru>
2023-11-11 22:39:31 +0400

    Добавлен полноценный поиск rule34.
    P.s. позже добавить парсинг тегов. не забыть бы.

--- cogs/dev_tools.py
+++ cogs/dev_tools.py
@@ -14,7 +14,7 @@ class DevTools(commands.Cog):
     @commands.slash_command(name="uptime", description="-- ONLY FOR DEVELOPER --")
     @commands.is_owner()
     async def uptime(self, inter):
-        dt = DT.datetime.fromisoformat(time.time() - start_time)
+        dt = DT.datetime.fromisoformat(f"{time.time() - start_time}")
         timestamp = int(dt.timestamp())
         embed = disnake.Embed(
             title="Аптайм бота.",
@@ -23,24 +23,6 @@ class DevTools(commands.Cog):
         embed.add_field(name="Bot Uptime", value=f"Бот был запущен <t:{timestamp}:R>"
                                                  f"Пинг - {(round(self.bot.latency * 1000))} ms")
         await inter.send(embed=embed)
-
-    @commands.Cog.listener()
-    async def on_slash_command_error(self, inter, error):
-        if isinstance(error, commands.Cooldown):
-            return
-        channel = self.bot.get_channel(1172161193612410972)
-        timestamp = int(DT.timestamp(DT.now()))
-        command = inter.permissions.value
-        if command == 562949953421311:
-            command = "562949953421311 - Все права"
-        if command == 8:
-            command = "8 - Администратор"
-        name = inter.application_command.name
-        embed = disnake.Embed(description=f"`💔` <t:{timestamp}:f> (<t:{timestamp}:R>) Последний вызов вызвал исключение:\n```cmd\n{error}\n```"
-                                          f"\nКод разрешений: \n```\n{command}\n```\nСервер: {inter.guild.name}\nПользователь: {inter.author.name}\nID сервера: {inter.guild.id}\nID участника: {inter.author.id}"
-                                          f"\n Команда: </{name}:{inter.data.id}>",
-                              color=disnake.Colour.red())
-        await channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(DevTools(bot))
---
+++ cogs/nsfw.py
@@ -0,0 +1,75 @@
+import disnake
+from disnake.ext import commands, components
+import random
+import aiohttp
+import json
+
+class NSFW(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(
+        description="😀 Развлечения | Выводит рандомную картинку для взрослых (поиск на rule34)"
+    )
+    async def nsfw(
+        self,
+        inter,
+        count: commands.Range[int, 0, 30] = commands.Param(1, description="Количество картинок для вывода"),
+        tags: str = commands.Param(
+            name="поиск",
+            description="Укажите тег для поиска, например: boy",
+        ),
+        id: int = commands.Param(
+            None,
+            description="Укажите ID публикации (при неверном ID будет показана рандомная картинка)",
+        ),
+    ):
+        if inter.channel.is_nsfw():
+            await inter.response.defer()
+
+            results = []
+
+            for _ in range(count):
+                if id is None:
+                    async with aiohttp.request(
+                        "GET",
+                        f"https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&limit=1000&tags={tags}&json=1",
+                    ) as resp:
+                        data = await resp.json()
+                else:
+                    async with aiohttp.request(
+                        "GET",
+                        f"https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&id={id}&json=1",
+                    ) as resp:
+                        data = await resp.json()
+
+                if data is None:
+                    return await inter.edit_original_message(
+                        embed=disnake.Embed(
+                            description="Результаты не найдены, попробуйте поискать с помощью таких тегов как:\n `cum penis anal gay` через пробел",
+                            color=0x2b2d31,
+                        ),
+                    )
+
+                keyses = len(data) - 1
+                rand = random.randint(0, keyses)
+                result = {"url": data[rand]["file_url"]}
+                results.append(result)
+
+            chunk_size = 10
+            for i in range(0, len(results), chunk_size):
+                chunk = results[i:i+chunk_size]
+                messages = []
+                for result in chunk:
+                    messages.append(f"Теги: {tags} \n{result['url']}")
+                await inter.send(content="\n".join(messages))
+
+        else:
+            embed = disnake.Embed(
+                description="Никакого NSFW в данном канале - только чистый и здоровый контент!",
+                color=0x2b2d31,
+            )
+            await inter.edit_original_message(embed=embed)
+
+def setup(bot):
+    bot.add_cog(NSFW(bot))
